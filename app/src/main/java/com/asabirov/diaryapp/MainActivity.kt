42dcf06f6d8eccb07d7a2b0d3c6f00730ccc90f2
Azat Sabirov <azatsabirov466@gmail.com>
2024-01-04 08:57:55 +0600

    created nav graph

--- app/src/main/java/com/asabirov/diaryapp/MainActivity.kt
+++ app/src/main/java/com/asabirov/diaryapp/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.asabirov.diaryapp.navigation.Screen
+import com.asabirov.diaryapp.navigation.SetupNavGraph
 import com.asabirov.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/asabirov/diaryapp/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.asabirov.diaryapp.util
+
+object Constants {
+
+    const val APP_ID = "application-0-mgwru"
+    const val CLIENT_ID = "194324128010-56r8celvbs1jubadfhv5llls02eli4oq.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
+}
