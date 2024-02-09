6d7ba3bceb8a064fddfe4801a017a1266eca96d0
Azat Sabirov <azatsabirov466@gmail.com>
2024-02-09 09:43:15 +0600

    added side effect to transparent status bar

--- app/src/main/java/com/asabirov/diaryapp/MainActivity.kt
+++ app/src/main/java/com/asabirov/diaryapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.asabirov.diaryapp.navigation.Screen
 import com.asabirov.diaryapp.navigation.SetupNavGraph
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiaryAppTheme {
                 val navController = rememberNavController()
